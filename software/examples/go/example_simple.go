edf153758f66926f7995166aae79ef08d47760b3
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:59:27 +0100

    go: Change import path.

--- software/examples/go/example_simple.go
+++ software/examples/go/example_simple.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/ipconnection"
-	"tinkerforge/ptc_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/ptc_bricklet"
 )
 
 const ADDR string = "localhost:4223"
