9973304a53ea08cdd40b8c98c70890dce3c22839
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:58:09 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,8 +1,5 @@
-use std::{io, error::Error};
-use std::thread;
-use tinkerforge::{ip_connection::IpConnection, 
-                  ptc_bricklet::*};
-
+use std::{error::Error, io, thread};
+use tinkerforge::{ip_connection::IpConnection, ptc_bricklet::*};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,23 +10,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let ptc = PtcBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-     let temperature_receiver = ptc.get_temperature_callback_receiver();
+    let temperature_receiver = ptc.get_temperature_callback_receiver();
 
-        // Spawn thread to handle received callback messages. 
-        // This thread ends when the `ptc` object
-        // is dropped, so there is no need for manual cleanup.
-        thread::spawn(move || {
-            for temperature in temperature_receiver {           
-                		println!("Temperature: {} °C", temperature as f32 /100.0);
-            }
-        });
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `ptc` object
+    // is dropped, so there is no need for manual cleanup.
+    thread::spawn(move || {
+        for temperature in temperature_receiver {
+            println!("Temperature: {} °C", temperature as f32 / 100.0);
+        }
+    });
 
-		// Set period for temperature receiver to 1s (1000ms).
-		// Note: The temperature callback is only called every second
-		//       if the temperature has changed since the last call!
-		ptc.set_temperature_callback_period(1000);
+    // Set period for temperature receiver to 1s (1000ms).
+    // Note: The temperature callback is only called every second
+    //       if the temperature has changed since the last call!
+    ptc.set_temperature_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
